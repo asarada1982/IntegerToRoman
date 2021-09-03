9501cb8c5fad422d775f8e47adc7e29614744eed
asarada1982 <52840528+asarada1982@users.noreply.github.com>
2021-09-03 12:19:13 -0600

    Included Javadoc for classes

--- src/main/java/com/asarada/romannumeral/IntToRomanApplication.java
+++ src/main/java/com/asarada/romannumeral/IntToRomanApplication.java
@@ -3,6 +3,10 @@ package com.asarada.romannumeral;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+/**
+ * This class has  no implementation, but an application entry access of this package.
+ */
+
 @ApplicationPath("/")
 public class IntToRomanApplication extends Application {
 
